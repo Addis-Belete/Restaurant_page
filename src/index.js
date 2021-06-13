230e0a765799fc4becb1dea6f61fe910d6536e24
Addis Belete <addis.bel@gmail.com>
2021-06-13 12:02:30 +0300

    Add Html skeleton and Style for home page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
+
 import _ from 'lodash';
+import './style.css';
+import Food from './images/food.jpeg';
 
 function component() {
-	const element = document.createElement('div');
+	const element = document.getElementById('content');
+	const header = document.createElement('header');
+	const nav = document.createElement('nav')
+	const home = document.createElement('a')
+	const menu = document.createElement('a')
+	const contact = document.createElement('a')
+	const logo = document.createElement('a')
+	const section = document.createElement('section')
+	const about = document.createElement('h2')
+	const description = document.createElement('p')
+	const footer = document.createElement('footer')
+	const copy = document.createElement('p')
 
-	// Lodash, currently included via a script, is required for this line to work
-	// Lodash, now imported by this script
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	logo.innerHTML = "Ethiopian Food";
+	home.innerHTML = "Home";
+	menu.innerHTML = "Menu";
+	menu.setAttribute("href", './menu.html');
+	contact.setAttribute('href', './contact.html');
+	home.setAttribute('href', './index.html');
+	logo.setAttribute('href', './index.html');
+	contact.innerHTML = "Contacts"
+	about.innerHTML = "About Ethiopian Food"
+	description.innerHTML = `"Ethiopian cuisine (Oromo: Nyaata Etiyoophiyaa; Amharic: የኢትዮጵያ ምግብ) 
+                             characteristically consists of vegetable and often very spicy meat dishes. ... 
+                             Ethiopians eat most of the time with their right hands,
+                             using pieces of injera to pick up bites of entrées and side dishes."`
+	copy.innerHTML = "&copy 2021";
+
+	nav.append(home, menu, contact);
+	header.append(logo, nav);
+	section.append(about, description);
+	footer.appendChild(copy)
+	logo.classList.add('logo');
+	element.append(header, section, footer);
+
 
 	return element;
 }
 
+
+
 document.body.appendChild(component());
+document.body.classList.add('page');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		index: './src/index.js',
+		menu: './src/menu.js',
+		contact: './src/contact.js',
+	},
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
 	},
 };
